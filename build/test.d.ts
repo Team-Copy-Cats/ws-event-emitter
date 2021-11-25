877ea2da1625b978b9e530a624d27213156a8b84
Unluckymichell <39999330+Unluckymichell@users.noreply.github.com>
2021-11-25 17:48:27 +0100

    Clean up

--- build/test.d.ts
+++
@@ -1,5 +0,0 @@
-declare var functionList: ((allow: () => void) => void)[];
-declare function parseFunctionList(list: string | any[], allowed: {
-    (): void;
-    (): void;
-}, i?: number): boolean;
